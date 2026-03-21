cbbaec3d6f52b8884baa12e68bf666d22d2601c4
BatuhanTorlak <torlakhanbatu@gmail.com>
2026-03-21 13:06:48 +0300

    Test sistemi düzenlendi
    Eklendi: Hata görmezden gelme
    Eklendi: Durum çıktısı renklendirmesi
    Çıkartıldı: Tek bir test dosyasını koşma

--- Tester.cs
+++ Tester.cs
@@ -1,3 +1,7 @@
+// Derleyiciyi susturmak için
+#pragma warning disable CS8618
+#pragma warning disable CS8605
+#pragma warning disable CA2200
 using System.Reflection;
 
 namespace ProjeIskender;
@@ -10,10 +14,12 @@ class Tester
         public MethodInfo[] methods;
     }
 
+    public bool ignoreErrors = true;
     private Testables[] tests;
     
     public void Init()
     {
+        Console.WriteLine($"[TestInfo] Errors are ignored");
         List<Testables> testables = new List<Testables>();
         Assembly assembly = Assembly.GetExecutingAssembly();
         var testableClasses = assembly.GetTypes().Where(x => x.GetCustomAttribute<TestableClass>() != null);
@@ -24,7 +30,16 @@ class Tester
             var testInit = x.GetMethod("TestInit");
             if (testInit is not null)
             {
-                testInit.Invoke(null, new object[] { });
+                try 
+                {
+                    testInit.Invoke(null, new object[] { });
+                }
+                catch (Exception e)
+                {
+                    Console.Write("[Test] ");
+                    ColoredWrite("Error", ConsoleColor.Red);
+                    Console.WriteLine($" on {x.Name} -> TestInit! Message: {e.Message}");
+                }
             }
             testables.Add(new Testables()
             {
@@ -36,7 +51,8 @@ class Tester
         tests = testables.ToArray();
     }
 
-    public static void Run(Type type)
+    // Kullanım Dışı!
+    public static void Run(Type type, bool ignoreErrors = true)
     {
         if (type.GetCustomAttribute<TestableClass>() == null)
         {
@@ -59,18 +75,36 @@ class Tester
         foreach (var x in methods)
         {
             var methodName = x.Name;
-            if ((bool)x.Invoke(null, new object[] { }))
+            try 
             {
-                Console.WriteLine($"[Test] {className} {methodName} Passed!");
-                passedCount++;
-                continue;
+                if ((bool)x.Invoke(null, new object[] { }))
+                {
+                    Console.WriteLine($"[Test] {className} {methodName} Passed!");
+                    passedCount++;
+                    continue;
+                }
+                Console.WriteLine($"[Test] {className} {methodName} Failed!");
+            } 
+            catch (Exception e) 
+            {
+                if (ignoreErrors) 
+                {
+                    Console.Write($"[Test] {className} {methodName} ");
+                    Console.ForegroundColor= ConsoleColor.Red;
+                    Console.WriteLine("Error!");
+                    Console.ResetColor();
+                }
+                else
+                {
+                    throw e;
+                }
             }
-            Console.WriteLine($"[Test] {className} {methodName} Failed!");
         }
         Console.WriteLine($"In all {className} tests, {passedCount}/{methods.Count()} Passed!");
     }
 
-    public static void Run<T>()
+    // Kullanım Dışı!
+    public static void Run<T>(bool ignoreErrors = true)
     {
         Run(typeof(T));
     }
@@ -84,15 +118,46 @@ class Tester
             foreach (var method in x.methods)
             {
                 var methodName = method.Name;
-                if ((bool)method.Invoke(null, new object[] { }))
+                try 
                 {
-                    Console.WriteLine($"[Test] {className} {methodName} Passed!");
-                    passedCount++;
-                    continue;
+                    if ((bool)method.Invoke(null, new object[] { }))
+                    {
+                        Console.Write($"[Test] {className} -> {methodName} ");
+                        ColoredWriteLine("Passed!", ConsoleColor.Green);
+                        passedCount++;
+                        continue;
+                    }
+                    Console.Write($"[Test] {className} -> {methodName} ");
+                    ColoredWriteLine("Failed!", ConsoleColor.Red);
                 }
-                Console.WriteLine($"[Test] {className} {methodName} Failed!");
+                catch (Exception e) 
+                {
+                    if (ignoreErrors) 
+                    {
+                        Console.Write($"[Test] {className} -> {methodName} ");
+                        ColoredWriteLine("Error!", ConsoleColor.DarkRed);
+                    }
+                    else 
+                    {
+                        throw e;
+                    }
+                }
             }
             Console.WriteLine($"In all {className} tests, {passedCount}/{x.methods.Count()} Passed!");
         }
+    }
+
+    private static void ColoredWriteLine(string text, ConsoleColor color)
+    {
+        Console.ForegroundColor = color;
+        Console.WriteLine(text);
+        Console.ResetColor();
+    }
+
+    private static void ColoredWrite(string text, ConsoleColor color)
+    {
+        Console.ForegroundColor = color;
+        Console.Write(text);
+        Console.ResetColor();
     }
 }
