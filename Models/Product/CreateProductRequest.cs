b0aec8b59b52ab1e77b2465f0403c1fac1c8e947
BatuhanTorlak <torlakhanbatu@gmail.com>
2026-04-26 23:45:39 +0300

    Birsürü şey eklendi. Bunu okuyacak bütün arkadaşlardan özür diliyorum Content-Type kontrol eden Middleware eklendi EntityFramework bağlantısı kuruldu Ngsql ayarlamaları yapıldı Dto sınıflarının tablo eşleştirmeleri yapıldı ProductService tamamen veritabanına bağlandı İlgili testler yapıldı Product Controller'ının 3 endpoint'i tamamlandı Resource servisi genelleştirildi ve Whitelist'e bulunun bütün dosya türlerini kabul edecek

--- Models/Product/CreateProductRequest.cs
+++ Models/Product/CreateProductRequest.cs
@@ -7,7 +7,4 @@ public class CreateProductRequest
 {
     [MaxLength(128), JsonPropertyName("name")] public required string Name { get; set; }
     [JsonPropertyName("price")] public required float Price { get; set; }
-    [JsonPropertyName("single-price")] public required bool SinglePrice { get; set; }
-    [JsonPropertyName("expire")] public required DateTime? ExpirationDate { get; set; } = null;
-    [JsonPropertyName("details")] public object? Details { get; set; } = null;
 }
